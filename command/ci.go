cb3e4710f94938f8cc6bb545c3deaf646e357e34
mluu <mluu@lancope.com>
2015-11-10 20:01:20 -0800

    implement ci task

--- command/ci.go
+++ command/ci.go
@@ -1,8 +1,28 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+  "github.com/codegangsta/cli"
+  "stash0.eng.lancope.local/dev-infrastructure/project-lifecycle/helpers"
+)
 
 func CmdCi(c *cli.Context) {
-  // Write your code here
-
+  CmdBootstrap(c)
+  if !helpers.LastCommandSuccess {
+    return
+  }
+  CmdTest(c)
+  if !helpers.LastCommandSuccess {
+    return
+  }
+  CmdPackage(c)
+  if !helpers.LastCommandSuccess {
+    return
+  }
+  if helpers.DockerComposeHasService("smoketest") {
+    CmdSmoketest(c)
+    if !helpers.LastCommandSuccess {
+      return
+    }
+  }
+  CmdPublish(c)
 }
