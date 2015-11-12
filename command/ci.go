0691db63a779bd3ac322bab5a345b5cd02b9586d
mluu <mluu@lancope.com>
2015-11-12 13:44:42 -0800

    call teardown after ci

--- command/ci.go
+++ command/ci.go
@@ -6,6 +6,14 @@ import (
 )
 
 func CmdCi(c *cli.Context) error {
+  // call 'teardown' at the end of 'ci', even if a failure occurs
+  // note, we don't want the LastCommandSuccess from teardown otherwise
+  // it would pollute the exit code.
+  defer func() {
+    orig := helpers.LastCommandSuccess
+    CmdTeardown(c)
+    helpers.LastCommandSuccess = orig
+  }()
   if err := CmdBootstrap(c); err != nil {
     return err
   }
---
+++ smoketest/spec/ci.feature
@@ -0,0 +1,11 @@
+Feature: ci task
+
+  Scenario: with a failing test
+    Given a file named "docker-compose.yml" with:
+    """yaml
+    test:
+      image: busybox
+      command: /bin/false
+    """
+    When I run `lc ci`
+    Then it should fail
