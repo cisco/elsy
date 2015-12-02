043d91846e6661f01e815819fcafccb18ccc9a05
Paul Cichonski <pcichonski@lancope.com>
2015-12-02 13:45:38 -0500

    log cmd about to run in CmdCi
    Currently it is not clear what phases are running in jenkins when running 'lc ci'.

--- command/ci.go
+++ command/ci.go
@@ -2,27 +2,38 @@ package command
 
 import (
   "github.com/codegangsta/cli"
+  "github.com/Sirupsen/logrus"
   "stash0.eng.lancope.local/dev-infrastructure/project-lifecycle/helpers"
 )
 
 // CmdCi runs ci loop
 func CmdCi(c *cli.Context) error {
   defer CmdTeardown(c)
+
+  logrus.Info("Running bootstrap")
   if err := CmdBootstrap(c); err != nil {
     return err
   }
+
   if helpers.DockerComposeHasService("test") {
+    logrus.Info("Running test")
     if err := CmdTest(c); err != nil {
       return err
     }
   }
+
+  logrus.Info("Running package")
   if err := CmdPackage(c); err != nil {
     return err
   }
+
   if helpers.DockerComposeHasService("smoketest") {
+    logrus.Info("Running smoketest")
     if err := CmdSmoketest(c); err != nil {
       return err
     }
   }
+
+  logrus.Info("Running publish")
   return CmdPublish(c)
 }
