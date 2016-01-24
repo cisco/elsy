63e4edaa998da110047147b52479a8b2d58b6cf9
Paul Cichonski <pcichons@lancope.com>
2016-01-24 14:27:49 -0500

    update view-template to process '--enable-scratch-volumes' flag
    Call example: 'lc --enable-scratch-volumes system view-template sbt'

--- command/system/view-template.go
+++ command/system/view-template.go
@@ -10,7 +10,7 @@ import (
 func CmdViewTemplate(c *cli.Context) error {
   if templateName := c.Args().First(); len(templateName) == 0 {
     return fmt.Errorf("view-template requires an argument of the template to view")
-  } else if yaml, err := template.Get(templateName, false); err != nil {
+  } else if yaml, err := template.Get(templateName, c.GlobalBool("enable-scratch-volumes")); err != nil {
     return err
   } else {
     fmt.Println(yaml)
