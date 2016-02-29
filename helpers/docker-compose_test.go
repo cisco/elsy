53ef7023e24143cc71ebe1b3ba972b0cff8561ee
Paul Cichonski <pcichonski@lancope.com>
2016-02-29 11:30:50 -0500

    fix docker-compose version checks
    Starting with docker-compose 1.5.2 build info was added after the version string.

--- helpers/docker-compose_test.go
+++ helpers/docker-compose_test.go
@@ -14,6 +14,7 @@ var testComposeVersionData = []struct{
   {"docker-compose version: 1.3.3\nCPython version: 2.7.9\nOpenSSL version: OpenSSL 1.0.1j 15 Oct 2014", "1.3.3", []int{1, 3, 3}},
   {"docker-compose 1.4.1", "1.4.1", []int{1, 4, 1}},
   {"docker-compose version: 1.5.0", "1.5.0", []int{1, 5, 0}},
+  {"docker-compose version 1.5.2, build 7240ff3", "1.5.2", []int{1, 5, 2}},
   {"docker-compose version: 1.6.0", "1.6.0", []int{1, 6, 0}},
 }
 func TestParseDockerCompseVersion(t *testing.T) {
