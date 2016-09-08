07290d4c40c03e05ebad17eb40b0efcff8941468
Paul Cichonski <pcichonski@lancope.com>
2016-09-08 17:03:48 -0400

    use local blackbox-test image

--- blackbox-test/spec_helper.rb
+++ blackbox-test/spec_helper.rb
@@ -1,6 +1,23 @@
 require 'tmpdir'
+require 'turnip/rspec'
+require 'turnip_formatter'
+require 'rspec_junit_formatter'
 
 RSpec.configure do |config|
+  # configure formatters
+  config.add_formatter 'documentation'
+  config.color = true
+  config.add_formatter RSpecTurnipFormatter, "/opt/project/test-reports/servicetest-report.html"
+  config.add_formatter RspecJunitFormatter, "/opt/project/test-reports/servicetest-rspec-junit.xml"
+
+  # fail when steps are unimplemented
+  config.raise_error_for_unimplemented_steps = true
+
+  # load base helpers
+  Dir.glob("blackbox-test/helpers/**/*_helper.rb") { |f| load f }
+  config.include EventuallyHelper, :type => :feature
+  Dir.glob("blackbox-test/steps/**/*_steps.rb") { |f| load f }
+
   config.before do
     @dir = Dir.mktmpdir("project-lifecycle-blackbox")
   end
